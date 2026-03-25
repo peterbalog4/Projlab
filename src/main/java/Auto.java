7f3b08827162569b795a965e89f34a9ffe8c3000
Biró Zalán <bzalan1025@gmail.com>
2026-03-25 23:49:23 +0100

    eskü nem raktam az előbb rossz helyre az összes .java filet

---
+++ src/main/java/Auto.java
@@ -0,0 +1,16 @@
+public class Auto extends Jarmu {
+
+    public void csuszik(){
+
+    }
+
+    public void kozlekedik(){
+
+    }
+
+    public void utkozik(Jarmu masikJarmu){
+
+    }
+    
+    
+}
